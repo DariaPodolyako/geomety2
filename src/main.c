410cb0ce1750ae65a8c09e18be57e1c2d965171d
Daria <ogloblik@yandex.ru>
2019-04-24 12:29:53 +0700

    Changed main.c

--- src/main.c
+++ src/main.c
@@ -9,17 +9,17 @@ int main()
     printf("enter the number of shapes=");
     scanf("%d", &n);
     float A[n];
-    for (i = 0; i < n; i++) {
-        printf("\n %d.circle=", i + 1);
+    for (i = 1; i <= n; i++) {
+        printf("\n %d.circle=", i);
         scanf("%d.circle(%f)", &i, &r);
         A[i] = r;
     }
 
-    for (i = 0; i < n; i++) {
+    for (i = 1; i <= n; i++) {
         s = square(A[i]);
         p = per(A[i]);
-        printf("\n %d.square=(%1.2f)", i + 1, s);
-        printf("\n %d.per=(%1.2f)", i + 1, p);
+        printf("\n %d.square=(%1.2f)", i, s);
+        printf("\n %d.per=(%1.2f)", i, p);
     }
 
     return 0;
