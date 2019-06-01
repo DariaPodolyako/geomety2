d716916539279f1969f488edb47a43bd1e224235
Dalia Kefir <dasha_pod01@mail.ru>
2019-06-01 20:01:52 +0700

    Try to make this lab to  build

--- src/main.c
+++ src/main.c
@@ -4,21 +4,23 @@
 #include <math.h>
 int main()
 {
-  int n, i;
+    int n, i;
     float r, x, y, s, p, pere;
     printf("enter the number of shapes=");
     scanf("%d", &n);
     float A[n];
     float X[n];
     float Y[n];
-    for (i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) 
+    {
         printf("\n %d.circle=", i);
         scanf("%f %f %f", &x, &y, &r);
-	X[i] = x;
-	Y[i] = y;
-	A[i] = r;
+    X[i] = x;
+    Y[i] = y;
+    A[i] = r;
     }
-    for (i = 1; i <= n-1; i++) {
+    for (i = 1; i <= n-1; i++) 
+    {
         s = square(A[i]);
         p = per(A[i]);
         pere = peres(A[i], X[i], Y[i], A[i+1], X[i+1], Y[i+1]);
@@ -26,6 +28,5 @@ int main()
         printf("\n %d.per=(%1.2f)", i, p);
         printf("\n %d,%d.peres=(%1.2f)", i, (i+1), pere);
     }
-
     return 0;
 }
